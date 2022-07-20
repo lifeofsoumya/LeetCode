785895ca034b0362090efda578cc74ff2783b044
Soumya Mondal <mondalsoumya2001@gmail.com>
2022-07-20 22:58:50 +0530

    Time: 27 ms (6.07%), Space: 14.1 MB (9.41%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -3,8 +3,7 @@ public:
     int finalValueAfterOperations(vector<string>& operations) {
         int init=0;
         for(auto str: operations){
-            if(str[1]=='-') init--;
-            else init++;
+            str[1]=='-' ? init--: init++;
         }
         return init;
     }
