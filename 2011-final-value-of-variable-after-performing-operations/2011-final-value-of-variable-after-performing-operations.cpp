f5ba8868a5bd5e491cd98a7eba4c8c6cd8999538
Soumya Mondal <mondalsoumya2001@gmail.com>
2022-07-20 22:59:13 +0530

    Time: 16 ms (38.10%), Space: 14.1 MB (57.60%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
         int init=0;
-        for(auto str: operations){
+        for(auto str:operations){
             str[1]=='-' ? init--: init++;
         }
         return init;
