f4e69999d77f9beae32a5dfd5f7a022d53738be0
Soumya Mondal <mondalsoumya2001@gmail.com>
2022-07-22 23:23:27 +0530

    Time: 16 ms (41.18%), Space: 15.3 MB (43.26%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.cpp
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
         string output = s;
-        for(int i=0; i<indices.size(); ++i){
+        for(int i=0; i<indices.size(); ++i)
             output[indices[i]]=s[i];
-        }
         return output;
     }
 };
