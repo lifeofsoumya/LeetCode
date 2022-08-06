ff6df3652c61e614a0c9284beaf32555460fd11d
Soumya Mondal <mondalsoumya2001@gmail.com>
2022-08-06 22:19:54 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (71.27%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -3,9 +3,9 @@ public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> res;
 	for (int i = 0; i < numRows; i++) {
-		vector<int> row(i + 1, 1);
-		for (int j = 1; j < i; j++) {
-			row[j] = res[i - 1][j] + res[i - 1][j - 1];
+		vector<int> row(i + 1, 1); //e.resize(int newSize, int AddNewval)
+		for (int j = 1; j < i; j++) { // value of j goes from 1st to the one before last
+			row[j] = res[i - 1][j] + res[i - 1][j - 1]; // add previous row (i-1)'s j-1 and j
 		}
 		res.push_back(row);
 	}
