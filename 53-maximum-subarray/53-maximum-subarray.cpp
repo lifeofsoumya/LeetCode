ad6209637511018e55ec12c9c99a9d204e06b4bc
Soumya Mondal <mondalsoumya2001@gmail.com>
2022-09-18 20:21:35 +0530

    Time: 237 ms (18.48%), Space: 67.8 MB (53.58%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -4,9 +4,9 @@ public:
         int sum = 0; 
         int maxn = INT_MIN;
         for( auto it : nums){
-            sum += it;
-            maxn = max(sum, maxn);
-            if(sum < 0) sum = 0;
+            sum += it; // add next number to sum
+            maxn = max(sum, maxn); // if sum is maximum then maxn = sum
+            if(sum < 0) sum = 0; // if sum is negative set sum to default 0
         }
         return maxn;
     }
