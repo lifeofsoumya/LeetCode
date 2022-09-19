727d91761e3620c02fc97ffd1f4d0581771aaec7
Soumya Mondal <mondalsoumya2001@gmail.com>
2022-09-19 22:48:14 +0530

    Time: 248 ms (15.43%), Space: 67.7 MB (90.45%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -6,7 +6,7 @@ public:
         for( auto it : nums){
             sum += it; // add next number to sum
             maxn = max(sum, maxn); // if sum is maximum then maxn = sum, which would be output
-            if(sum < 0) sum = 0; // if sum is negative set sum to default 0 : which is triggering point, making it O of n complexity
+            if(sum < 0) sum = 0; // if sum is negative set sum to default 0 : which is triggering point, making it O of n complexity : When sum of two elements is less than 0, there's no point of including them to subarray coz they will anyway decrease the sum, so better start the res subarray after them, so set sum = 0
         }
         return maxn;
     }
