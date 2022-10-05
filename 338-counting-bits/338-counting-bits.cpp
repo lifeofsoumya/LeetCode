b77eabe5e37a2330aa2306d605f025bbe8a4151d
Soumya Mondal <mondalsoumya2001@gmail.com>
2022-10-05 22:46:33 +0530

    Time: 12 ms (35.05%), Space: 7.9 MB (47.48%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int>ans (n+1, 0);
+        vector<int>ans (n+1, 0); // n+1 items all default to 0
         for(int i = 1; i <= n; i++)
-            ans[i] = ans[i/2] + (i&1);
+            ans[i] = ans[i/2] + (i&1); // “i&1” evaluates to “true” if “i” is and odd number else it returns “false”
         return ans;
     }
 };
