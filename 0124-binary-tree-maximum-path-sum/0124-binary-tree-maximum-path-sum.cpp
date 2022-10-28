281247649ace3c7c074eac82b24dd1ddebd6cce0
Soumya Mondal <mondalsoumya2001@gmail.com>
2022-10-28 15:03:45 +0530

    Time: 61 ms (26.40%), Space: 28.7 MB (6.85%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -13,12 +13,13 @@ class Solution {
 public:
     int solve(TreeNode* node, int &res){
         if(!node) return 0;
-        int leftBS = node->val + solve(node->left, res);
+        int leftBS = node->val + solve(node->left, res); // left branch sum using recursion
         int rightBS = node->val + solve(node->right, res);
         
-        res = max({res, node->val, leftBS, rightBS, leftBS+rightBS-node->val});
+        // either max we've found already, or this node is max, or left subtree is max, or right one, or the tree starting from this node is max
+        res = max({res, node->val, leftBS, rightBS, leftBS+rightBS-node->val}); 
         
-        return max({leftBS, rightBS, node->val});
+        return max({leftBS, rightBS, node->val}); //Return the max branch Sum
     }
     int maxPathSum(TreeNode* root) {
         int res = INT_MIN;
