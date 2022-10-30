8da4df09a4220969bfd89dd6d79b6519ec7bd6b1
Soumya Mondal <mondalsoumya2001@gmail.com>
2022-10-30 18:55:28 +0530

    Time: 19 ms (92.81%), Space: 26.4 MB (27.45%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        map<int, int> inMap;
+        for(int i = 0; i < inorder.size(); i++){
+            inMap[inorder[i]] = i;
+        }
+        
+        TreeNode* root = buildTree(preorder, 0, preorder.size()-1, inorder, 0, inorder.size()-1, inMap);
+        return root;
+    }
+    
+    TreeNode* buildTree(vector<int> &preorder, int preStart, int preEnd, vector<int> &inorder, int inStart, int inEnd, map<int, int> &inMap){
+        if(preStart > preEnd || inStart > inEnd) return NULL;
+        
+        TreeNode* root = new TreeNode(preorder[preStart]); // from first elem of preorder find root, create node of it
+        
+        int inRoot = inMap[root->val]; // using value we find index of root in inorder array
+        int numsLeft = inRoot - inStart; // find number of elem in left of root in inorder arr
+        
+        root->left = buildTree(preorder, preStart+1, preStart+numsLeft, inorder, inStart, inRoot-1, inMap);
+        root->right = buildTree(preorder, preStart+numsLeft+1, preEnd, inorder, inRoot+1, inEnd, inMap);
+        
+        return root;
+    }
+};
