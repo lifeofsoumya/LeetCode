70fe46b9b4d2d75b357e0bb2112638d56df7fdf7
Soumya Mondal <mondalsoumya2001@gmail.com>
2022-12-24 11:45:07 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (82.12%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    void Sum(vector<int>& candidates, int target, vector<vector<int> >& res, vector<int>& r, int i)
+    void Sum(vector<int>& arr, int target, vector<vector<int> >& res, vector<int>& r, int i)
     {
         if(target == 0){
             res.push_back(r);
             return;
         }
         
-        while(i <  candidates.size() && target - candidates[i] >= 0){
-            r.push_back(candidates[i]);
-            Sum(candidates,target - candidates[i],res,r,i);
-            i++;
+        while(i <  arr.size() && target - arr[i] >= 0){
+            r.push_back(arr[i]);
+            Sum(arr,target - arr[i],res,r,i);
             r.pop_back();
+            i++;
         }
     }
 
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
-        candidates.erase(unique(candidates.begin(),candidates.end()),candidates.end()); // remove non unique      
+    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
+        sort(arr.begin(),arr.end());
+        arr.erase(unique(arr.begin(),arr.end()),arr.end()); // remove non unique      
         vector<int> r;
         vector<vector<int> > res;        
-        Sum(candidates,target,res,r,0);       
+        Sum(arr, target, res, r, 0);       
         return res;
     }
 };
