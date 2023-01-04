0327e01e687e50ae66a762f4d4578c0ff1e6ba88
Soumya Mondal <mondalsoumya2001@gmail.com>
2023-01-04 14:27:22 +0530

    Time: 601 ms (22.83%), Space: 103.8 MB (45.14%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -10,10 +10,8 @@ public:
         for(auto x: m){
             int t = x.second;
             if(t == 1) return -1;
-            if(t%3==0) count+=t/3;
-            else count+=(t/3)+1;
-            // count += t / 3;
-            // if(t % 3 != 0) count++;
+            if(t%3==0) count+=t/3; // if task is multiple of three then we can do three task in t/3 times
+            else count+=(t/3)+1; // any nyumber of task takes t/3 time, rest suppose if 2, it takes another step
         }
         return count;
     }
