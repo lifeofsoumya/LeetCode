95a2504606bae11b21e56c35fb85bb872bc021a3
Soumya Mondal <mondalsoumya2001@gmail.com>
2023-01-05 17:44:30 +0530

    Time: 211 ms (13.37%), Space: 34.1 MB (22.23%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) 
-    {
-        std::vector<int> res;
-        std::priority_queue<std::pair<int, int>> pq;
-
-        for (auto it : arr)
-        {
-            int closest = abs(it - x);
-            //std::cout << closest << " : " << it << "\n";
-            pq.push(std::make_pair(closest, it));
-
-            if (pq.size() > k)
-            {
-                pq.pop();     
-            }
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pair<int, int>> pq;
+        for(auto el : arr){
+            pq.push({abs(el-x), el});
+            if(pq.size()>k) pq.pop();
         }
-
-        //std::cout << "\nDebug queue : \n";
-        while(!pq.empty())
-        {
+        vector<int> res;
+        while(!pq.empty()){
             res.push_back(pq.top().second);
-            //std::cout << pq.top().first << " : " << pq.top().second << "\n";
             pq.pop();
         }
-
-        std::sort(res.begin(), res.end());
+        sort(res.begin(), res.end());
         return res;
     }
 };
