c0833b69691af5f7b12cdb85b4e0fe3b13e30841
Soumya Mondal <mondalsoumya2001@gmail.com>
2023-01-24 12:09:03 +0530

    Time: 255 ms (5.00%), Space: 108.6 MB (5.00%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int totalCost=0,totalFuel=0,n=cost.size();
-        for(int i=0;i<n;i++){
-            totalCost+=cost[i];
-            totalFuel+=gas[i];
+        int totalGas = 0, totalCost = 0, n = cost.size();
+        for(int i = 0 ; i < n; i++){
+            totalGas += gas[i]; // total gas we have
+            totalCost += cost[i]; // total gas we need
         }
-        if(totalFuel<totalCost){
-            return -1;
-        }
-        int currFuel=0,start=0;  
-        for(int i=0;i<n;i++){
-            if(currFuel<0){
-                start=i;
-                currFuel=0;
-            }
-            currFuel+=(gas[i]-cost[i]);
+        if(totalGas < totalCost) return -1;
+        int currGas = 0, start= 0;
+        for(int i = 0; i < n ; i++){
+            currGas += gas[i] - cost[i]; // e.g 3-5 = -2
+            if(currGas < 0){ // enters if as -2 < 0
+                start = i+1; // imagine to starting from next index, when if doesn't occur means we've started from the next index
+                currGas = 0; // again set currGas = 0
+            }           
         }
         return start;
     }
