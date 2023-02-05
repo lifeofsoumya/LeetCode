d9f9f7d0f83d134aa8f990a2f5173d9e3414ea9e
Soumya Mondal <mondalsoumya2001@gmail.com>
2023-02-06 01:27:48 +0530

    Time: 41 ms (25.01%), Space: 29.9 MB (18.20%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -6,28 +6,27 @@ class Solution {
         return true;
     }
 public:
-    bool checkInclusion(string s1, string s2) {
-        if(s2.size()<s1.size()) return false;
-        vector<int> freqS1(26, 0);
-        for(char c: s1) freqS1[c-'a']++;
-        
-        vector<int> freqS2(26, 0);
-        int i=0, j=0;
-        
-        while(j<s2.size()){
-            freqS2[s2[j]-'a']++;
-            
-            if(j-i+1==s1.size()){
-                if(areVectorsEqual(freqS1, freqS2)) return true;
-            }
-            
-            if(j-i+1<s1.size()) j++;
-            else{
-                freqS2[s2[i]-'a']--;
-                i++;
-                j++;
-            }
+    
+bool checkInclusion(string s1, string s2) {
+    if (s2.size() < s1.size()) return false;
+    vector<int> freqS1(26, 0);
+    for (char c : s1) freqS1[c - 'a']++;
+
+    vector<int> freqS2(26, 0);
+    int i = 0, j = 0;
+
+    while (j < s2.size()) {
+        freqS2[s2[j] - 'a']++;
+
+        if (j - i + 1 == s1.size()) {
+            if (areVectorsEqual(freqS1, freqS2)) return true;
+            freqS2[s2[i] - 'a']--;
+            i++;
         }
-        return false;
+        j++;
     }
+    return false;
+}
+
+
 };
