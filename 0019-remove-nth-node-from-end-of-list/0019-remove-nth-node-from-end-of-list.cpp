9b9749483e421d436dd1221ee920eb179800a9c8
Soumya Mondal <mondalsoumya2001@gmail.com>
2023-05-26 22:25:12 +0530

    Time: 7 ms (38.35%), Space: 10.6 MB (95.46%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -15,10 +15,10 @@ public:
         for(int i = 0; i < n; i++) fast = fast->next; // traverse n node but from 1st
         if(!fast) return head->next; // if fast's at last, the n = length
         while(fast->next){
-            fast = fast->next;
-            slow = slow->next;
+            fast = fast->next; // fast previously got head start of n length
+            slow = slow->next; // so slow'll go at same pace but be behing 'n' from fast
         }
-        slow->next = slow->next->next;
+        slow->next = slow->next->next; // finally modifying 
         return head;
     }
 };
