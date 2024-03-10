edfb97836c3ff1b26e1faaff985b721a3b2fd9d5
Soumya Mondal <mondalsoumya2001@gmail.com>
2024-03-11 00:32:57 +0530

    Time: 1228 ms (90.83%), Space: 0B (100.00%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,9 +1,7 @@
-# Write your MySQL query statement below
-
 SELECT
     p.product_id,
-    COALESCE(ROUND(SUM(price * units) / SUM(units), 2), 0) AS average_price
+    IFNULL(ROUND(SUM(price * units) / SUM(units), 2), 0) AS average_price
 FROM Prices p
-LEFT JOIN UnitsSold us ON p.product_id = us.product_id
+LEFT JOIN UnitsSold us ON p.product_id = us.product_id #left join to keep all the prices even if not sold, just show avg price 0 for that case
    AND us.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY p.product_id;
