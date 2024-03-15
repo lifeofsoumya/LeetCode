effaccd88b6500c97eb914e23af574443c88af16
Soumya Mondal <mondalsoumya2001@gmail.com>
2024-03-15 23:59:17 +0530

    Time: 713 ms (44.61%), Space: 0B (100.00%) - LeetHub

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,1 @@
+SELECT query_name, ROUND(AVG(rating / position), 2) AS quality, ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100 / COUNT(*), 2) AS poor_query_percentage FROM queries WHERE query_name is NOT NULL GROUP BY query_name
