910a4327fdf9437275f6ed6fc74d43598e80c114
Soumya Mondal <mondalsoumya2001@gmail.com>
2024-03-19 13:47:58 +0530

    Time: 2167 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -2,7 +2,7 @@
 
 SELECT ROUND(COUNT(DISTINCT player_id) / (SELECT COUNT(DISTINCT player_id) FROM ACTIVITY), 2) AS fraction
 FROM Activity
-WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY)) # whoever had 1 day gap of game 
+WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY)) # from each day sub 1, & check if it matches min date 
 IN (
     SELECT player_id, MIN(event_date) FROM Activity  GROUP BY player_id
 )
