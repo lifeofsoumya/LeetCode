67766ffaf9b2100e1e2d8343818605337d2c341e
Soumya Mondal <mondalsoumya2001@gmail.com>
2024-04-12 18:09:57 +0530

    Time: 939 ms (68.43%), Space: 0B (100.00%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,4 +1,4 @@
-select distinct Num as ConsecutiveNums
-from Logs l
-where (l.id + 1, Num) in (select * from Logs) and (l.id + 2, Num) in (select * from Logs)
+SELECT DISTINCT num as ConsecutiveNums
+FROM Logs l
+WHERE (l.id + 1, num) IN (SELECT * FROM Logs) AND (l.id + 2, num) IN (SELECT * FROM Logs)
 
