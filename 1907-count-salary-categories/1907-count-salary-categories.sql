6db3b09652450badcf7298cb9a8a5cc98d2433c1
Soumya Mondal <mondalsoumya2001@gmail.com>
2024-04-19 23:45:55 +0530

    Time: 2301 ms (82.83%), Space: 0B (100.00%) - LeetHub

---
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -0,0 +1,12 @@
+SELECT "Low Salary" AS category, SUM(income < 20000) AS accounts_count # comparison returns 1, so keep summing
+FROM Accounts
+
+UNION
+
+SELECT  "Average Salary" AS category, SUM(income BETWEEN 20000 AND 50000) AS accounts_count
+FROM Accounts
+
+UNION
+
+SELECT "High Salary" AS category, SUM(income > 50000) AS accounts_count
+FROM Accounts
