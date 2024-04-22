cf9a5882c64e1216dab03528650ced6f453e989b
Soumya Mondal <mondalsoumya2001@gmail.com>
2024-04-23 00:01:13 +0530

    Time: 651 ms (68.08%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,3 +1,4 @@
-SELECT ROW_NUMBER() OVER() id, student
-FROM seat
-ORDER BY IF(MOD(id, 2) = 0, id-1, id+1)
+SELECT CASE WHEN id = (SELECT MAX(id) FROM Seat) AND id % 2 = 1 THEN id WHEN id % 2 = 1 THEN id + 1 ELSE id - 1 END AS id, student
+FROM Seat
+ORDER BY id
+
