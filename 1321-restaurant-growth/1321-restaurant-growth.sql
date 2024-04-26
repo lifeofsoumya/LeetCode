facc308220999d4b26c49ccdeb8ac8f468c5cb33
Soumya Mondal <mondalsoumya2001@gmail.com>
2024-04-26 21:17:50 +0530

    Time: 944 ms (16.05%), Space: 0B (100.00%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,10 +1,36 @@
-SELECT
-    a.visited_on AS visited_on,
-    SUM(b.day_sum) AS amount,
-    ROUND(AVG(b.day_sum), 2) AS average_amount
+WITH cte AS
+(
+    SELECT
+    visited_on
+,   SUM(amount) amount
+
 FROM
-  (SELECT visited_on, SUM(amount) AS day_sum FROM Customer GROUP BY visited_on ) a,
-  (SELECT visited_on, SUM(amount) AS day_sum FROM Customer GROUP BY visited_on ) b
-WHERE DATEDIFF(a.visited_on, b.visited_on) BETWEEN 0 AND 6
-GROUP BY a.visited_on
-HAVING COUNT(b.visited_on) = 7 
+    Customer
+
+GROUP BY
+    visited_on
+),
+cte1 AS
+(
+SELECT
+    visited_on,
+    SUM(amount) OVER(ORDER BY visited_on ROWS BETWEEN 6 preceding and CURRENT ROW) amount,
+    ROUND(AVG(amount) OVER(ORDER BY visited_on ROWS BETWEEN 6 preceding and CURRENT ROW),2) average_amount,
+    ROW_NUMBER()OVER(ORDER BY visited_on) RN
+FROM
+    cte
+)
+
+SELECT
+    visited_on
+,   amount
+,   average_amount
+
+FROM
+    cte1
+
+WHERE
+    RN > 6
+
+ORDER BY
+    visited_on
