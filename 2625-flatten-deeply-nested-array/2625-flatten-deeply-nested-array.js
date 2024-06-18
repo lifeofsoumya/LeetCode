cbaa592e9b5350d153018164dd10e86c694ce7e0
Soumya Mondal <mondalsoumya2001@gmail.com>
2024-06-18 21:35:12 +0530

    Time: 219 ms (5.95%), Space: 78.5 MB (39.65%) - LeetHub

---
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+var flat = function (arr, n) {
+    if(n==0) return arr;
+    const res = [];
+    for(let i = 0; i < arr.length; i++){
+        if(Array.isArray(arr[i])) res.push(...flat(arr[i], n-1))
+        else res.push(arr[i])
+    }
+    return res;
+};
