f6772b1f105029659d2013fdff42647c4698def6
Soumya Mondal <mondalsoumya2001@gmail.com>
2024-07-11 18:42:49 +0530

    Time: 70 ms (47.43%), Space: 51.5 MB (82.84%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    let totalSum = 0
+    for(let i = 0; i < nums.length; i++) {
+        totalSum += nums[i]
+    }
+    let leftSum = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (leftSum * 2 == totalSum - nums[i])
+            return i;
+        leftSum += nums[i]
+    }
+    return -1 
+}
