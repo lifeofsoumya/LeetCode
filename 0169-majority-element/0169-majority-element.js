b51bb5db11c32bbc13b7c816c0f6e6366bda4568
Soumya Mondal <mondalsoumya2001@gmail.com>
2024-07-18 19:02:40 +0530

    Time: 49 ms (95.95%), Space: 52.8 MB (34.45%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    const hash = {};
+    let res = 0;
+    let majority = 0;
+    
+    for (let n of nums) {
+        hash[n] = 1 + (hash[n] || 0);
+        if (hash[n] > majority) {
+            res = n;
+            majority = hash[n];
+        }
+    }
+
+    return res;
+};
